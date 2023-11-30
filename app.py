666a7adc72c4781c03da2f3f6e6fd3e1bea39b07
varunshankarhoskere <varunhoskere@gmail.com>
2023-12-01 02:55:30 +0530

    yay video streaming works

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, url_for, send_from_directory
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # Check if 'output.mp4' exists
+    if os.path.exists('output.mp4'):
+        return render_template('display_video.html')
+    else:
+        return 'No fall detected'
+
+@app.route('/video')
+def video():
+    return send_from_directory('.', 'output.mp4')
+
+if __name__ == '__main__':
+    app.run(debug=True)
